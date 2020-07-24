f60918f3355c91b56701d7af6413272c2818c14f
SamuelLJackson <severett29@gmail.com>
2020-07-24 14:53:27 -0400

    remove unnecessary files

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ console.log(
 connection.connect();
 const filename = 'result.xlsx';
 
-connection.query('select start_weight, end_weight, zone from rates where client_id=1240', function (error, results, fields) {
+connection.query("select start_weight as 'Start Weight', end_weight as 'End Weight', zone as Zone from rates where client_id=1240", function (error, results, fields) {
   if (error) throw error;
   convert(results, filename);
 });
